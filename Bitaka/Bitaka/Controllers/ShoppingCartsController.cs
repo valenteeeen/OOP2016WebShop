93a42dcb0affd6c71ba21e75a5c5f0d5a40d55c1
limitlessmindgirl <dimitrieva_martina@hotmail.com>
2016-06-05 20:00:26 +0300

    ShoppingCart

---
+++ Bitaka/Bitaka/Controllers/ShoppingCartsController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Bitaka.Models;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNet.Identity;
+
+namespace Bitaka.Controllers
+{
+    public class ShoppingCartsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: ShoppingCarts
+        public ActionResult Index(List<Products> p)
+        {
+            var id = User.Identity.GetUserId();
+            var shoppingCarts = db.ShoppingCarts.Where(s => s.ApplicationUser.Id==id);
+            db.Products.ToList();
+           
+            
+            return View(shoppingCarts.ToList());
+
+         }
+
+        // GET: ShoppingCarts/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ShoppingCart shoppingCart = db.ShoppingCarts.Find(id);
+            if (shoppingCart == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shoppingCart);
+        }
+
+        // GET: ShoppingCarts/Create
+        public ActionResult Create()
+        {
+            
+            return View();
+        }
+
+        // POST: ShoppingCarts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,Name,Price,Description,Image")] ShoppingCart shoppingCart)
+        {
+            if (ModelState.IsValid)
+            {
+                var id = User.Identity.GetUserId();
+                shoppingCart.ApplicationUser = db.Users.Find(id);
+                db.ShoppingCarts.Add(shoppingCart);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            
+            return View(shoppingCart);
+        }
+
+       
+
+        // GET: ShoppingCarts/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ShoppingCart shoppingCart = db.ShoppingCarts.Find(id);
+            if (shoppingCart == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.id = new SelectList(db.IdentityUsers, "Id", "UserName", shoppingCart.id);
+            return View(shoppingCart);
+        }
+
+        // POST: ShoppingCarts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,Name,Price,Description,Image")] ShoppingCart shoppingCart)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(shoppingCart).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.id = new SelectList(db.IdentityUsers, "Id", "UserName", shoppingCart.id);
+            return View(shoppingCart);
+        }
+
+        // GET: ShoppingCarts/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ShoppingCart shoppingCart = db.ShoppingCarts.Find(id);
+            if (shoppingCart == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shoppingCart);
+        }
+
+        // POST: ShoppingCarts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            ShoppingCart shoppingCart = db.ShoppingCarts.Find(id);
+            db.ShoppingCarts.Remove(shoppingCart);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Bitaka/Bitaka/Models/ShoppingCart.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Bitaka.Models
+{
+    public class ShoppingCart
+    {
+        [Key, ForeignKey("ApplicationUser")]
+        public string id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+
+
+        public virtual ApplicationUser ApplicationUser { get; set; }
+        public virtual ICollection<Products> Products { get; set; }
+
+
+
+
+        public int Count { get; set; }
+
+        public int ProductId { get; set; }
+    }
+}
